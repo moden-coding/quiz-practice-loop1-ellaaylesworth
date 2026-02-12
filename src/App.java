6134ac874b47b5d7282f5ab008d6d797fc333c12
ella <29eaylesworth@gannacademy.org>
2026-02-12 11:41:21 -0500

    All done!

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 /**
  *  Write a Java program that reads an unspecified number of integers from the user 
  *  until the user enters -1. For each number, indicate whether it is odd or even. 
@@ -9,8 +10,28 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        int countOfpositivenumbers = 0;
+        int countOfnegativenumbers = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            
+            int number = Integer.valueOf(scanner.nextLine());
+            int remainder = number % 2;
+                if (number == -1) {
+                    break;
+                }
 
+                if (remainder == 0) {
+                    System.out.println("Even!");
+                    countOfpositivenumbers += 1;
+                } else {
 
+                    System.out.println("Odd!");
+                    countOfnegativenumbers += 1;
+                }
+            }
+            System.out.println("The number of negative numbers was " + countOfnegativenumbers);
+            System.out.println("The number of positive numbers was " + countOfpositivenumbers);
+        }
 
     }
-}
